d6bf9668458482699206a28170b5c5e36a905647
azamatbolat <azamat.bolat@yandex.com>
2023-08-02 00:25:09 +0100

    hehe

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
-let popupNode = document.querySelector('.popup');
-let editProfile = document.querySelector('.profile__edit-profile');
-let popupNodeClose = document.querySelector('.popup__closed');
+let popupNode = document.querySelector(".popup");
+let editProfile = document.querySelector(".profile__edit-profile");
+let popupNodeClose = document.querySelector(".popup__closed");
 
-let profileName = document.querySelector('.profile__title');
-let profileJob = document.querySelector('.profile__subtitle');
+let profileName = document.querySelector(".profile__title");
+let profileJob = document.querySelector(".profile__subtitle");
 
-let popupForm = popupNode.querySelector('.popup__form');
+let popupForm = popupNode.querySelector(".popup__form");
 
-let inputName = popupForm.querySelector('.popup__item_name');
-let inputJob = popupForm.querySelector('.popup__item_job');
-let saveProfile = popupForm.querySelector('.popup__button');
+let inputName = popupForm.querySelector(".popup__item_name");
+let inputJob = popupForm.querySelector(".popup__item_job");
+let saveProfile = popupForm.querySelector(".popup__button");
 
-function setPopupInputValue() { 
-    inputName.value = profileName.textContent.trim();
-    inputJob.value = profileJob.textContent.trim();
-} 
-
-function setProfileDesc() {
-    profileName.textContent = inputName.value;
-    profileJob.textContent = inputJob.value;
+function setPopupInputValue() {
+  inputName.value = profileName.textContent.trim();
+  inputJob.value = profileJob.textContent.trim();
 }
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    setProfileDesc();
+function setProfileDesc() {
+  profileName.textContent = inputName.value;
+  profileJob.textContent = inputJob.value;
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  setProfileDesc();
 }
 
 function popupOpen() {
-    setPopupInputValue();
-    popupNode.classList.add('popup_opened');
-    console.log('включил');
-} 
+  setPopupInputValue();
+  popupNode.classList.add("popup_opened");
+  console.log("включил");
+}
 
 function popupClose() {
-    popupNode.classList.remove('popup_opened');
-       console.log('выключил');
-    } 
+  popupNode.classList.remove("popup_opened");
+  console.log("выключил");
+}
 
 function setProfileDescClosePopup() {
-    setProfileDesc();
-    popupClose();
-    console.log('выключил');
+  setProfileDesc();
+  popupClose();
+  console.log("выключил");
+}
 
-}    
-
-editProfile.addEventListener ('click', popupOpen);
-popupNodeClose.addEventListener ('click', popupClose);
-popupForm.addEventListener('submit', formSubmitHandler);
-saveProfile.addEventListener('click', setProfileDescClosePopup);
+editProfile.addEventListener("click", popupOpen);
+popupNodeClose.addEventListener("click", popupClose);
+popupForm.addEventListener("submit", formSubmitHandler);
+saveProfile.addEventListener("click", setProfileDescClosePopup);
