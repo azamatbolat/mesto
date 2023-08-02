6b33d7220c495a5699063f607c43e3107e8be59b
azamatbolat <azamat.bolat@yandex.com>
2023-08-02 01:53:05 +0100

    tt

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let popupNode = document.querySelector(".popup");
 let editProfile = document.querySelector(".profile__edit-profile");
-let popupNodeClose = document.querySelector(".popup__closed");
+let popupNodeClose = popupNode.querySelector(".popup__closed");
 
 let profileName = document.querySelector(".profile__title");
 let profileJob = document.querySelector(".profile__subtitle");
@@ -24,6 +24,7 @@ function setProfileDesc() {
 function formSubmitHandler(evt) {
   evt.preventDefault();
   setProfileDesc();
+  popupClose();
 }
 
 function popupOpen() {
@@ -37,13 +38,6 @@ function popupClose() {
   console.log("выключил");
 }
 
-function setProfileDescClosePopup() {
-  setProfileDesc();
-  popupClose();
-  console.log("выключил");
-}
-
 editProfile.addEventListener("click", popupOpen);
 popupNodeClose.addEventListener("click", popupClose);
 popupForm.addEventListener("submit", formSubmitHandler);
-saveProfile.addEventListener("click", setProfileDescClosePopup);
